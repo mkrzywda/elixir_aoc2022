2fc22351a0b902b8d6ad911572eab25c9666be09
Maciej Krzywda <maciej.krzywda.ext@olx.pl>
2022-12-01 08:49:40 +0100

    Day 1: Calorie Counting

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,26 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+  	args
+	    |> String.split("\n\n", trim: true)
+      |> Enum.map(&parse_elves/1)
+	    |> Enum.map(&Enum.sum/1)
+      |> Enum.max()
   end
 
-  def part2(args) do
+  defp parse_elves(elves_stuff) do
+    elves_stuff
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
   end
+
+
+  def part2(args) do
+    args
+      |> String.split("\n\n", trim: true)
+      |> Enum.map(&parse_elves/1)
+	    |> Enum.map(&Enum.sum/1)
+      |> Enum.sort(:desc)
+      |> Enum.take(3)
+      |> Enum.sum()
+end
 end
