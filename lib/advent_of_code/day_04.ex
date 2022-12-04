7613291631d14e60efe6e226c8a328e6d3f0f4e6
Maciej Krzywda <maciej.krzywda.ext@olx.pl>
2022-12-04 12:55:30 +0100

    Day 4: Camp Cleanup

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day04 do
   def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+    |> Enum.filter(fn [a, b] -> MapSet.subset?(a, b) || MapSet.subset?(b, a) end)
+    |> Enum.count()
   end
 
-  def part2(args) do
+  defp parse_line(string) do
+    string
+    |> String.split(",")
+    |> Enum.map(fn s ->
+      [h, l] = s |> String.split("-") |> Enum.map(&String.to_integer/1)
+      MapSet.new(h..l)
+    end)
+  end
+    def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+    |> Enum.filter(fn [a, b] -> MapSet.intersection(a, b) |> Enum.count() != 0 end)
+    |> Enum.count()
   end
 end
