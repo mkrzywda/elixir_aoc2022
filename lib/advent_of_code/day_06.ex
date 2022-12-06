740fb700d255d6eed9d6e66ab438270673900551
Maciej Krzywda <maciej.krzywda.ext@olx.pl>
2022-12-06 10:22:58 +0100

    Day 6: Tuning Trouble

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,17 @@
 defmodule AdventOfCode.Day06 do
   def part1(args) do
+    args
+    |> String.graphemes()
+    |> Enum.chunk_every(4, 1)
+    |> Enum.find_index(fn chars -> Enum.uniq(chars) == chars end)
+    |> Kernel.+(4)
   end
 
   def part2(args) do
+    args
+    |> String.graphemes()
+    |> Enum.chunk_every(14, 1)
+    |> Enum.find_index(fn chars -> Enum.uniq(chars) == chars end)
+    |> Kernel.+(14)
   end
 end
