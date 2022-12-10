ea64723b59ebd33546f432041aca6a684ec2f1be
Maciej Krzywda <maciej.krzywda.ext@olx.pl>
2022-12-10 19:15:41 +0100

    Day 10: Cathode-Ray Tube

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,48 @@
 defmodule AdventOfCode.Day10 do
+
   def part1(args) do
+    args
+    |> get_cycles()
+    |> Enum.with_index()
+    |> Enum.drop(19)
+    |> Enum.take_every(40)
+    |> Enum.map(fn {x, cycle} -> x * (cycle + 1) end)
+    |> Enum.sum()
+  end
+
+  defp get_cycles(input) do
+    input
+    |> String.split("\n", trim: true)
+    |>Enum.reduce({[], 1}, fn
+       "addx " <> num, {accumulated_nums, acc_x} -> add_x(num, accumulated_nums, acc_x)
+       "noop", {accumulated_nums, acc_x} -> append_x(accumulated_nums, acc_x)
+    end)
+    |> elem(0)
+    |> Enum.reverse()
+  end
+
+  defp add_x(num, accumulated_nums, acc_x) do
+    {[acc_x, acc_x | accumulated_nums], acc_x + String.to_integer(num)}
+  end
+
+  defp append_x(accumulated_nums, acc_x) do
+    {[acc_x | accumulated_nums], acc_x}
   end
 
   def part2(args) do
+    args
+    |> get_cycles()
+    |> Enum.chunk_every(40)
+    |> Enum.map(fn chunk ->
+      chunk
+      |> Enum.with_index()
+      |> Enum.map(fn
+        {acc_x, idx} when abs(acc_x - idx) < 2 -> '█'
+        _ -> '.'
+      end)
+      |> Enum.join()
+    end)
+    |> Enum.join("\n")
+    |> IO.puts()
   end
 end
